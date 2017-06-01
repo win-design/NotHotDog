80fa705be6b38b2902da6425803360aaca2051a9
Anass GMS <smartt24@gmail.com>
2017-06-01 05:52:01 +0000

    first
    first

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='nothotdog',
-    version='0.0.1',
+    version='0.1.1',
     packages=['nothotdog'],
     include_package_data=True,
     install_requires=[
